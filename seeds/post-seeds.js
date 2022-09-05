e3645c39e0bf0e428fbc71b6a9a0f13c88ecbf99
jaquel <jaquelrey@gmail.com>
2022-09-05 19:47:52 -0400

    removed the dastardly spaces from seeds

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,13 +2,13 @@ const { Post } = require("../models");
 
 const postData = [
   {
-   " title": "Taskmaster goes public!",
+   "title": "Taskmaster goes public!",
     "post_content":
       "Taskinator is a task-tracker app that will allow you to organize your personal to-do list items by clicking and dragging them into categories like To Do, In Progress, and Completed.",
     "user_id": 3,
   },
   {
-   " title": "Zoo Keepr reaches 1 million subscribers!",
+   "title": "Zoo Keepr reaches 1 million subscribers!",
     "post_content":
       "The local zoo has received funding to build a new online catalog, and they've asked to create a web server for a front-end application they’re developing, called Zoo Keepr. This site’s data will be stored on the server. This will allow animal enthusiasts to access the data from different locations and browsers without needing to download it to their device",
     "user_id": 1,
@@ -23,10 +23,10 @@ const postData = [
     "title": "Tech Blog has been released!",
     "post_content":
       "A CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well. This site will be built from scratch and deployed to Heroku. The app will follow MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication",
-   " user_id": 5,
+   "user_id": 5,
   },
   {
-   " title": "Just Tech News goes public!",
+   "title": "Just Tech News goes public!",
     "post_content":
       "Just Tech News—a tech news website where users can post, upvote, and comment on links to news articles. Use Sequelize, an object-relational mapping (ORM) library, to simplify your MySQL queries, add password hashing so that users can create secure passwords, and connect your application to JawsDB, a MySQL add-on for Heroku",
     "user_id": 4,
